f7b7fc09df779119e8e20630a16881f6fbbc395d
StepStepi4 <st.rakovyts2021@gmail.com>
2026-04-19 12:58:30 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'skillhub-cache-auto';
+const CACHE_ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.jpg'];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => { if(k !== CACHE_NAME) return caches.delete(k); }))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.includes('script.google.com')) return;
+  
+  event.respondWith(
+    fetch(event.request).then((res) => {
+      return caches.open(CACHE_NAME).then(cache => {
+        cache.put(event.request, res.clone());
+        return res;
+      });
+    }).catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html')))
+  );
+});
